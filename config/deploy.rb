fe0de298995a2d3da32acde2061a2790e40fea2b
Walter Neto <walterdf@gmail.com>
2017-01-28 11:55:27 -0200

    Correção base

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,13 @@
 # config valid only for current version of Capistrano
-lock "3.7.1"
+lock "3.7.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "sistema-curso"
+set :repo_url, "git@github.com:walterdf/sistema-curso.git"
+
+set :deploy_to, '/home/deploy/sistema-curso'
+
+append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
